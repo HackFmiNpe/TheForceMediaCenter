216ff8049f661aa02a015d86581f097c64e575ec
Philip Yankov <duxmaster@gmail.com>
2015-12-20 04:46:23 +0200

    Fix bugs -- what's left -- f/b + volume up/down

--- realServer/Client.java
+++ realServer/Client.java
@@ -1,18 +1,35 @@
 package realServer;
 
 import java.io.IOException;
+
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Client {
   public static void main(String[] args) throws Exception {
-//    String str = "{\"gesture\":\"down\",\"full_name\":\"NFS\"}";
-    String str = "{\"gesture\":\"down\"}";
+//    String str = "{\"gesture\":\"open\"}";
+    
+    // implement go back
+    
+//    String str = "{\"gesture\":\"down\"}";
+//    String str = "{\"gesture\":\"up\"}";
+//    String str = "{\"gesture\":\"right\"}";
+//    String str = "{\"gesture\":\"left\"}";
+//    
+//    String str = "{\"gesture\":\"open\",\"full_name\":\"avatarm.mp4\"}";
+//    
+//    String str = "{\"gesture\":\"vol_up\"}";
+//    String str = "{\"gesture\":\"vol_down\"}";
+//    
+//    String str = "{\"gesture\":\"forward\"}";
+    String str = "{\"gesture\":\"backward\"}";
+//    
+//    String str = "{\"gesture\":\"close\"}";
 
     Socket socket = null;
     OutputStreamWriter osw;
     try {
-        socket = new Socket("192.168.0.100", 1234);
+        socket = new Socket("192.168.0.123", 1234);
         osw =new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
         osw.write(str, 0, str.length());
         osw.flush();
--- realServer/Server.java
+++ realServer/Server.java
@@ -13,6 +13,10 @@ import org.json.JSONObject;
 
 public class Server {
 
+  static {
+    System.setProperty("java.awt.headless", "false");
+  }
+
   private static final String GESTURE_KEY = "gesture";
   private static final String FULL_NAME_KEY = "full_name";
   static boolean hasSelection = false;
@@ -22,6 +26,7 @@ public class Server {
   }
 
   static void server() {
+//     executeOnCommandLine("export DISPLAY=:0");
     try (ServerSocket server = new ServerSocket(1234)) {
       System.out.println("Waiting for connection");
       while (true) {
@@ -90,6 +95,20 @@ public class Server {
           // hit enter
           r.keyPress(KeyEvent.VK_ENTER);
           r.keyRelease(KeyEvent.VK_ENTER);
+
+          try {
+            Thread.sleep(3000);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          System.out.println("Folder opened.");
+
+          // hit right
+          r.keyPress(KeyEvent.VK_RIGHT);
+          r.keyRelease(KeyEvent.VK_RIGHT);
+
+          r.keyPress(KeyEvent.VK_DOWN);
+          r.keyRelease(KeyEvent.VK_DOWN);
         }
       } else {
         // open movie file
@@ -119,9 +138,12 @@ public class Server {
   private static void closeMovie(Robot r) {
     // call omxplayer close - play -- omxplayer --name fullName
     r.keyPress(KeyEvent.VK_ALT);
-    r.keyPress(KeyEvent.VK_ALT);
+    r.keyPress(KeyEvent.VK_F4);
+    r.keyRelease(KeyEvent.VK_ALT);
     r.keyRelease(KeyEvent.VK_F4);
-    r.keyRelease(KeyEvent.VK_F4);
+    if (hasSelection) {
+      hasSelection = false;
+    }
   }
 
   private static void volumeDown(Robot r) {
@@ -152,19 +174,27 @@ public class Server {
   private static void openMyComputer(Robot r) {
     // open My Computer
     executeOnCommandLine("nautilus /");
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("My computer opened.");
 
     // hit right
     r.keyPress(KeyEvent.VK_RIGHT);
     r.keyRelease(KeyEvent.VK_RIGHT);
+
+    hasSelection = true;
   }
 
   private static void executeOnCommandLine(String command) {
     try {
       Runtime rt = Runtime.getRuntime();
       Process p = rt.exec(command);
-      if (p.exitValue() != 0) {
-        System.err.println("Open My computer fails " + p.exitValue());
-      }
+      // if (p.exitValue() != 0) {
+      // System.err.println("Open My computer fails " + p.exitValue());
+      // }
     } catch (IOException e) {
       e.printStackTrace();
     }
